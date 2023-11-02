467469ab7cf15e7939aeabd7328a4891d9aa60ce
rkia33445 <rkia199.khayoussef@gmail.com>
2023-11-01 20:56:03 -0400

    i have added the feature planet1

--- src/test/java/SolarSystem/main.java
+++ src/test/java/SolarSystem/main.java
@@ -12,5 +12,11 @@ public class main {
         Sun.name="panther";
         Sun.color="black";
         Sun.temp="-1223124kelvin";
+        //        PLanet1
+        Planet1 plato=new Planet1();
+        plato.color="green";
+        plato.name="abracadabra";
+        plato.temp="10000000k";
+        plato.radius="800000km";
     }
 }
